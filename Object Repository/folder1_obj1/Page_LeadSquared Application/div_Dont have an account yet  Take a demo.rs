00feee0fc992347c07ed6479881b5c0bc10103d7
shwetha.u <shwetha.u@LSQLAP-7058.lsq.net>
2022-06-20 15:07:53 +0530

    comiit1

---
+++ Object Repository/folder1_obj1/Page_LeadSquared Application/div_Dont have an account yet  Take a demo.rs
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Dont have an account yet  Take a demo</name>
+   <tag></tag>
+   <elementGuidId>e454ab80-6eda-47a1-bb5d-c3b9025695ba</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.signup-div</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='login-form-container']/div[2]/div/div/div/div[3]/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>22c859c1-0596-4b8d-8626-0414a618566f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>signup-div</value>
+      <webElementGuid>4ed611e5-ca1a-4809-974e-c19f38d3b8b6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            Don't have an account yet ? Take a demo
+        </value>
+      <webElementGuid>b9676ce4-e623-4e2a-8060-ff8717e98214</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;login-form-container&quot;)/div[@class=&quot;login-form-body&quot;]/div[@class=&quot;login-form-body-wrapper&quot;]/div[@class=&quot;login-form-body-container&quot;]/div[@class=&quot;lsq-sigin-verify-email&quot;]/div[@class=&quot;lsq-signin-footer&quot;]/div[@class=&quot;signup-div&quot;]</value>
+      <webElementGuid>4294a391-a869-4c5f-8c4f-7b8fbe23c158</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='login-form-container']/div[2]/div/div/div/div[3]/div</value>
+      <webElementGuid>67b659b7-9d0e-4aea-9d50-d83970628f90</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Next'])[1]/following::div[1]</value>
+      <webElementGuid>190da4a1-d1b6-4f73-a962-e740e67593e1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Forgot Password?'])[1]/following::div[2]</value>
+      <webElementGuid>554bf9c8-7241-4782-adb8-f0d5cb0a7e5a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='© Copyright 2022, MarketXpander Services Pvt. Ltd. All rights reserved.'])[1]/preceding::div[4]</value>
+      <webElementGuid>080b7c84-b06e-4c2e-80cf-a27c23d7d3ac</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div[3]/div</value>
+      <webElementGuid>344fb0d8-a5d4-4d2c-81d2-5fbce195eeef</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = concat(&quot;
+            Don&quot; , &quot;'&quot; , &quot;t have an account yet ? Take a demo
+        &quot;) or . = concat(&quot;
+            Don&quot; , &quot;'&quot; , &quot;t have an account yet ? Take a demo
+        &quot;))]</value>
+      <webElementGuid>0e979aa9-3964-4e39-bd0a-db0a82fd08bb</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
